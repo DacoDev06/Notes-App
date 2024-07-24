3389b4dbd65cd2f9fb2d15837b7287ef8a1f775b
Dacoop <juancho1601@hotmail.com>
2024-07-23 22:37:23 -0500

    [add] deleteNote, some styles

--- src/APP/index.js
+++ src/APP/index.js
@@ -3,16 +3,46 @@ import { Title } from '../Components/Title';
 import {NotesGrid} from '../Components/NotesGrid'
 import {Note} from '../Components/Note'
 import { AddNoteButton } from '../Components/AddNoteButton';
+import React from 'react';
 
+const notesList = [
+  {id:1,title:"Nota 1",desciption:"texto cualquiera para la descripcion",color:"blue",isCompleted:false},
+  {id:2,title:"Nota 2",desciption:"Algun que hacer en nuestra hija",color:"white",isCompleted:false},
+  {id:3,title:"Nota 3",desciption:"texto cualquiera para la descripcion",color:"black",isCompleted:false},
+  {id:4,title:"Nota 3",desciption:"texto cualquiera para la descripcion",color:"red",isCompleted:false},
+]
 
 function App() {
+  const [notes,setNotes] = React.useState(notesList)
+  // const [notes,setNotes] = React.useState(JSON.parse(localStorage.getItem('notes'))||[])
+
+  const saveNotes = (newNotes)=>{
+    setNotes(newNotes)
+    localStorage.setItem('notes',JSON.stringify(newNotes))
+  }
+
+
+  const deleteNotes = (id)=>{
+    let newNotes = [...notes]
+    const index = newNotes.findIndex(note => note.id === id)
+    newNotes.splice(index,1)
+    saveNotes(newNotes)
+  }
+
+
   return (
     <div className="App">
         <Title/>
         <NotesGrid>
-          <Note/>
-          <Note/>
-          <Note/>
+          {notes.map(note=>
+            <Note
+              key={note.id}
+              title={note.title}
+              description={note.desciption}
+              deleteNotes={()=>deleteNotes(note.id)}
+              color={note.color}
+            />
+           )}
         </NotesGrid>
         <AddNoteButton/>
     </div>
--- src/Components/Note/index.js
+++ src/Components/Note/index.js
@@ -1,21 +1,23 @@
 import './Note.css'
 
 
-function Note(){
+function Note({title,description,deleteNotes , color}){
     return(
-        <div className='note'>
+        <div className='note' style={{backgroundColor:color,boxShadow:`1px 1px 12px ${color}`}}>
             <div className='note-info'>
                 <p className='note-info-title'>
-                    Soy el titulo ASDASDASDASDASD
+                    {title}
                 </p>
-                <span>
+                <span className='note-edit'>
                     J
                 </span>
             </div>
             <div className="note-description">
-                SOY LA DESCRIPCION DE LA NOTA
+                {description}
             </div>
-            <span className='note-delete'>
+            <span 
+            className='note-delete'
+            onClick={deleteNotes}>
                 X
             </span>
         </div>
